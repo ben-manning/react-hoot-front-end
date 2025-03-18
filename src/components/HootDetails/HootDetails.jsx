e7b8945aad6668a2d9dff9e79fb27ee784fdc688
Ben Manning <bmanning@me.com>
2025-03-18 14:51:29 -0400

    complete comment creation feature

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from 'react';
 import * as hootService from '../../services/hootService';
+import CommentForm from "../CommentForm/CommentForm";
 
 
 
@@ -20,6 +21,11 @@ const HootDetails = () => {
     fetchHoot();
   }, [hootId]);
 
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await hootService.createComment(hootId, commentFormData);
+    setHoot({ ...hoot, comments: [...hoot.comments, newComment ]});
+  };
+
   if (!hoot) return <main>Loading...</main>
   
   return (
@@ -37,6 +43,7 @@ const HootDetails = () => {
       </section>
       <section>
       <h2>Comments</h2>
+      <CommentForm handleAddComment={handleAddComment} />
 
         {!hoot.comments.length && <p>There are no comments.</p>}
 
